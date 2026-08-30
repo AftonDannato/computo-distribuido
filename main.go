159bb06d739876c96ece48519763bbd80f705ac0
AftonDannato <dbsg.2000@gmail.com>
2026-08-29 18:00:37 -0600

    Conexión Go-Postgre

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+
+	"github.com/AftonDannato/computo-distribuido/controllers"
+	"github.com/AftonDannato/computo-distribuido/models"
+)
+
+func main() {
+	dsn := "host=localhost port=5432 user=postgres password=postgres dbname=security_monitor sslmode=disable"
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	eventModel := models.NewEventModel(db)
+	eventController := controllers.NewEventController(eventModel)
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /events", eventController.GetEvents)
+
+	log.Println("Servidor escuchando en http://localhost:8080")
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
+}
